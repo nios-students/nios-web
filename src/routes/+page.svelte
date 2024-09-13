fb95ab3518b64344bed6237d45673011a8212dc3
Ur <Aqeelur.m+github@gmail.com>
2024-09-13 23:11:10 +0300

    Update +page.svelte
    try no.2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <div id="main">
 	<h1>Welcome to NIOS Students</h1>
-	  <p>NIOS-Students  is a project by「NIOS Unofficial」 a community of  NIOS students.</p>
+	  <p>NIOS-Students  is a project by「NIOS Unofficial」 a community of  NIOS students</p>
 	<h2>Contributors:</h2>
 
 	<div id="contributors">
